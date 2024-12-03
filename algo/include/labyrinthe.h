3db95488f4e6bcafb09fc5a0a4c08db43746c344
nlevasseur <nathan.levasseur@inssa-rouen.fr>
2024-12-03 11:07:53 +0100

    #20 développement de labyrinthe.h

--- algo/include/labyrinthe.h
+++ algo/include/labyrinthe.h
@@ -17,37 +17,55 @@ typedef struct {
     LAB_Case lesConnections[4];
     int entree;
     int sortie;
+    int largeur;
 } LAB_Labyrinthe;
 
 /**
-* @fn LAB_Labyrinthe labyrinthe()
+* @fn LAB_creerLabyrinthe LAB_creerLabyrinthe()
 * @brief Créer un labyrinthe
 * @return un labyrinthe
 */
-LAB_Labyrinthe LAB_labyrinthe();
+LAB_Labyrinthe LAB_creerLabyrinthe();
 
 /**
-* @fn int* obtenirCasesAccessibles(LAB_Case)
+* @fn int* LAB_obtenirCasesAccessibles(LAB_Case)
 * @brief Obtenir la liste des cases accessibles d'une case 
+* @param LAB_Labyrinthe le labyrinthe
 * @param LAB_Case une case
-* @return liste de cases
+* @return liste de cases accessibles
 */
-LAB_Case* LAB_obtenirCasesAccessibles(LAB_Case);
+LAB_Case* LAB_obtenirCasesAccessibles(LAB_Labyrinthe, LAB_Case);
 
 /**
-* @fn LAB_Case obtenirEntree(LAB_Labyrinthe)
-* @brief Obtenir la case sortie d'un labyrinthe
+* @fn LAB_Case LAB_obtenirEntree(LAB_Labyrinthe)
+* @brief Obtenir la case entrée d'un labyrinthe
 * @param LAB_Labyrinthe un labyrinthe
 * @return la case d'entrée
 */
-LAB_Case LAB_obtenirEntree(LAB_Labyrinthe);
+unsigned int LAB_obtenirEntree(LAB_Labyrinthe);
 
 /**
-* @fn LAB_Case obtenirSortie(LAB_Labyrinthe)
+* @fn LAB_Case LAB_obtenirSortie(LAB_Labyrinthe)
 * @brief Obtenir la case sortie d'un labyrinthe
 * @param LAB_Labyrinthe un labyrinthe
 * @return la case de sortie
 */
-LAB_Case LAB_obtenirSortie(LAB_Labyrinthe);
+unsigned int LAB_obtenirSortie(LAB_Labyrinthe);
+
+/**
+* @fn LAB_Case LAB_obtenirLargeur(LAB_Labyrinthe)
+* @brief Obtenir la largeur d'un labyrinthe
+* @param LAB_Labyrinthe un labyrinthe
+* @return la largeur
+*/
+unsigned int LAB_obtenirLargeur(LAB_Labyrinthe);
+
+/**
+* @fn LAB_Case LAB_iemeCase(LAB_Labyrinthe, unsigned int)
+* @brief Obtenir la ième case d'un labyrinthe
+* @param LAB_Labyrinthe un labyrinthe
+* @return la ième case
+*/
+LAB_Case LAB_iemeCase(LAB_Labyrinthe, unsigned int);
 
 #endif
