6839c7e1ff6ddb0a00baa8190216153d74f999e3
nlevasseur <nathan.levasseur@inssa-rouen.fr>
2024-12-08 14:33:56 +0100

    #20 Développement du labyrinthe

--- algo/src/labyrinthe.c
+++ algo/src/labyrinthe.c
@@ -2,3 +2,33 @@
 #include <stdlib.h>
 
 #include "labyrinthe.h"
+
+LAB_Labyrinthe LAB_creerLabyrinthe(unsigned int l, LAB_Case entree, LAB_Case sortie) {
+    LAB_Labyrinthe lab;
+    lab.entree = entree;
+    lab.sortie = sortie;
+    lab.largeur = largeur;
+    return lab;
+}
+
+unsigned int LAB_obtenirLargeur(LAB_Labyrinthe lab) {
+    return lab.largeur;
+}
+
+LAB_Case LAB_obtenirEntree(LAB_Labyrinthe lab) {
+    return lab.entree;
+}
+
+LAB_Case LAB_obtenirSortie(LAB_Labyrinthe lab) {
+    return lab.sortie;
+}
+
+unsigned int LAB_iemeCase(LAB_Labirynthe lab, unsigned int iCase) {
+    LAB_Case res = lab.lesConnections[iCase];
+    return res;
+}
+
+LAB_Case Lab_obtenirCasesAccessibles(LAB_Labyrinthe lab, unsigned int iCase) {
+    LAB_Case res = LAB_iemeCase(lab, iCase);
+    return res;
+}
