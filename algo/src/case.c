0d05fe8105ec8794e4a63fc9af1180a4a0a89480
Michel Vespier <michel.vespier@insa-rouen.fr>
2024-12-10 10:27:20 +0100

    #21 avancement case.h (création, setter)

---
+++ algo/src/case.c
@@ -0,0 +1,26 @@
+#include "case.h"
+
+int CASE_obtenirNumeroCase(CASE_Case c)
+{
+    return c.Numero;
+}
+
+int CASE_obtenirHaut(CASE_Case c)
+{
+    return c.Haut;
+}
+
+int CASE_obtenirBas(CASE_Case c)
+{
+    return c.Bas;
+}
+
+int CASE_obtenirGauche(CASE_Case c)
+{
+    return c.Gauche;
+}
+
+int CASE_obtenirDroite(CASE_Case c)
+{
+    return c.Droite;
+}
