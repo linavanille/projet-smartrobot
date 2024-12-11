291d495f801902eeb8b3ddb26817fa2fb7054035
Michel Vespier <michel.vespier@insa-rouen.fr>
2024-12-11 16:36:06 +0100

    #27 Déplacement

--- elec/src/test/test_deplacement.c
+++ elec/src/test/test_deplacement.c
@@ -1,12 +1,11 @@
 #include <wiringPi.h>
 #include <sys/time.h>
-#include "initElec.h"
 #include "motorsPi.h"
 #include "pinsRef.h"
 
 int main()
 {
-    initialisationGPIO();
+    MTR_initialisationMoteur(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
     MTR_avancer(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
     delay(2);
     MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
