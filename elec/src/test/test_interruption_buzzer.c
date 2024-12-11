dd004d5f7f50c7b350379eda3f6e8294b8a27e11
Michel Vespier <michel.vespier@insa-rouen.fr>
2024-12-11 13:58:29 +0100

    #27 tests interruption

--- elec/src/test/test_interruption_buzzer.c
+++ elec/src/test/test_interruption_buzzer.c
@@ -1,9 +1,11 @@
 #include "buzzer.h"
 #include "ultrason.h"
 #include "interruption.h"
+#include <wiringPi.h>
 
 int main()
 {
+    wiringPiSetupGpio();
     BUZZ_setup();
     USON_init();
     STOP_loop(BUZZ_loop, BUZZ_loop);
