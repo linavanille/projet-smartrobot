9bd1dde3990c5c0b2d4dec65042b85bf2a6a9d2c
Michel Vespier <michel.vespier@insa-rouen.fr>
2024-12-11 16:44:23 +0100

    correction

--- elec/src/test/test_deplacement.c
+++ elec/src/test/test_deplacement.c
@@ -1,22 +1,31 @@
 #include <wiringPi.h>
 #include <sys/time.h>
+#include <stdio.h>
 #include "motorsPi.h"
 #include "pinsRef.h"
 
 int main()
 {
+    int nb = 0;
     wiringPiSetupGpio();
     MTR_initialisationMoteur(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
+    printf("%d\n", nb++);
     MTR_avancer(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
+    printf("%d\n", nb++);
     delay(2);
     MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
+    printf("%d\n", nb++);
     delay(2);
     MTR_tournerDroite(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
+    printf("%d\n", nb++);
     delay(2);
     MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
+    printf("%d\n", nb++);
     delay(2);
     MTR_tournerGauche(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
+    printf("%d\n", nb++);
     delay(2);
     MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
+    printf("%d\n", nb++);
     return 0;
 }
