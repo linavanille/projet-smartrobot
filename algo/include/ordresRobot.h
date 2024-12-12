dd10200eb5fc3462242ec70d4443d953a5ef5659
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2024-12-12 15:42:55 +0100

    adaptation des noms de variable aux conventions

--- algo/include/ordresRobot.h
+++ algo/include/ordresRobot.h
@@ -11,48 +11,48 @@
 #include "case.h"
 
 /**
- * @struct Position
+ * @struct ORD_Position
  * @brief Représente une position dans le labyrinthe
  */
-typedef CASE_Case Position;
+typedef CASE_Case ORD_Position;
  
 /**
- * @enum Orientation
+ * @enum ORD_Orientation
  * @brief Représente la façon dont est orienté le robot par rapport au labyrinthe 
  */
-typedef enum {Nord,Sud,Est,Ouest,Invalide} Orientation; 
+typedef enum {Nord,Sud,Est,Ouest,Invalide} ORD_Orientation; 
 
 /**
- * @enum Ordre
+ * @enum ORD_Ordre
  * @brief Représente les 3 types d'ordres que le robot peut exécuter 
  */
-typedef enum {AV,TG,TD} Ordre;
+typedef enum {AV,TG,TD} ORD_Ordre;
 
 /**
  * @struct Liste
  * @brief Représente une liste de cases
  */
 typedef struct Liste {
-	Position** cases;  
+	ORD_Position** cases;  
 	unsigned int taille;  
 } Liste;
 
-Position* ORD_avancer(Position* position, Orientation orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
+ORD_Position* ORD_avancer(ORD_Position* position, ORD_Orientation orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
 
-Position* ORD_tournerGauche(Position* position, Orientation* orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
+ORD_Position* ORD_tournerGauche(ORD_Position* position, ORD_Orientation* orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
 
-Position* ORD_tournerDroite(Position* position, Orientation* orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
+ORD_Position* ORD_tournerDroite(ORD_Position* position, ORD_Orientation* orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
 
-Position* ORD_initialisation(LAB_Labyrinthe* labyrinthe, Orientation* orientation);
+ORD_Position* ORD_initialisation(LAB_Labyrinthe* labyrinthe, ORD_Orientation* orientation);
 
-Orientation ORD_calculerOrientation(Position* caseActuelle, Position* caseSuivante, unsigned int largeurLabyrinthe);
+ORD_Orientation ORD_calculerOrientation(ORD_Position* caseActuelle, ORD_Position* caseSuivante, unsigned int largeurLabyrinthe);
 
-bool ORD_estIntersection(Position* position, LAB_Labyrinthe* labyrinthe);
+bool ORD_estIntersection(ORD_Position* position, LAB_Labyrinthe* labyrinthe);
 
-bool ORD_estVirage(Position* position, Orientation orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
+bool ORD_estVirage(ORD_Position* position, ORD_Orientation orientation, LAB_Labyrinthe* labyrinthe, unsigned int largeurLabyrinthe);
 
-Ordre* ORD_obtenirOrdres(Position** chemin, unsigned int tailleChemin, LAB_Labyrinthe* labyrinthe);
+ORD_Ordre* ORD_obtenirOrdres(ORD_Position** chemin, unsigned int tailleChemin, LAB_Labyrinthe* labyrinthe);
 
-bool estAccessible(Position* positionActuelle, Position* positionVoisine);
+bool estAccessible(ORD_Position* positionActuelle, ORD_Position* positionVoisine);
 
 #endif // ORDRES_ROBOT_H
