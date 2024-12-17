97ef404288de00f9b7601a2a9e7c29a554fb2fa1
Tom Guenot <tguenot@iti-mahr213-02.ad.insa-rouen.fr>
2024-12-17 09:54:19 +0100

    #22 debbugage

--- elec/src/robot/robot.c
+++ elec/src/robot/robot.c
@@ -15,7 +15,7 @@
 
 #define TEMPS_ATTENTE           200000      //temps d'attente en ms du début du virage 
 #define TEMPS_VIRAGE            100         //temps en ms d'un virage
-#define TEMPS_ARRIVE_INTER      1400000      //temps d'attente en ms pour mettre le point de pivot à l'intersection
+#define TEMPS_ARRIVE_INTER      700000      //temps d'attente en ms pour mettre le point de pivot à l'intersection
 
 #endif
 
@@ -26,7 +26,7 @@ void ROBOT_avancer(ROBOT_EtatDAvancement* etat){
     int nb = 0;
     MTR_avancer(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
     while(!estSurInter){
-        estSurInter = CPTR_estSurUneIntersection(CPTR_LIGNE_CENTRE, CPTR_LIGNE_GAUCHE, CPTR_LIGNE_DROIT);
+        /*estSurInter = CPTR_estSurUneIntersection(CPTR_LIGNE_CENTRE, CPTR_LIGNE_GAUCHE, CPTR_LIGNE_DROIT);
         if (estSurInter){
             sens[9] = 'I';
         }
@@ -64,7 +64,7 @@ void ROBOT_avancer(ROBOT_EtatDAvancement* etat){
             sens[6] = ' ';
             sens[7] = ' ';
         }
-        LCD_Write(sens, vide);
+        LCD_Write(sens, vide);*/
 
         if (USON_obtenirDistance()<=10){
                     ROBOT_urgence();
@@ -89,8 +89,8 @@ void ROBOT_avancer(ROBOT_EtatDAvancement* etat){
             }
             MTR_avancer(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
         }
-        delayMicroseconds(TEMPS_ARRIVE_INTER);
     }
+    delayMicroseconds(TEMPS_ARRIVE_INTER);
     *etat = Intersection; 
 }
 
