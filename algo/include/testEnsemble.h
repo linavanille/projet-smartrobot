ceccc1eab39078aaa214487a87f1cd18770a0416
Amsatou <amsatou.diop@insa-rouen.fr>
2024-12-17 08:52:27 +0100

    fichiers tests collection ensemble

---
+++ algo/include/testEnsemble.h
@@ -0,0 +1,10 @@
+#ifndef __TEST_ENSEMBLE__H
+#define __TEST_ENSEMBLE__H
+#include "ensemble.h"
+/**
+*@brief pour afficher l'ensemble de cases
+*/
+void afficherEnsemble(Ensemble* ensembleDeCases);
+
+
+#endif 
