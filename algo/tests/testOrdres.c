90584e47d544ff6adae723789d93e7b973067c67
lelomaribouya <lina.el_omari_bouya@insa-rouen.fr>
2024-12-19 07:04:42 +0100

    création de virages et intersections fictifs pour les tests

--- algo/tests/testOrdres.c
+++ algo/tests/testOrdres.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ordres.h"
-#include "labyrinthe.h"
-#include "case.h"
+#include "ordresRobot.h"
 
 LAB_Labyrinthe* creerLabyrintheExemple() {
 	LAB_Labyrinthe* labyrinthe = malloc(sizeof(LAB_Labyrinthe));
@@ -11,14 +9,58 @@ LAB_Labyrinthe* creerLabyrintheExemple() {
 
 	for (int i = 0; i < 9; i++) {
 		labyrinthe->lesConnections[i].Numero = i;
-		labyrinthe->lesConnections[i].Haut = (i >= 3) ? i - 3 : -1;
-		labyrinthe->lesConnections[i].Bas = (i < 6) ? i + 3 : -1;
-		labyrinthe->lesConnections[i].Gauche = (i % 3 != 0) ? i - 1 : -1;
-		labyrinthe->lesConnections[i].Droite = (i % 3 != 2) ? i + 1 : -1;
+		labyrinthe->lesConnections[i].Haut = -1;
+		labyrinthe->lesConnections[i].Bas = -1;
+		labyrinthe->lesConnections[i].Gauche = -1;
+		labyrinthe->lesConnections[i].Droite = -1;
 	}
 
-	labyrinthe->entree = 0;
-	labyrinthe->sortie = 7;
+	// Définition des connexions
+	labyrinthe->lesConnections[0].Droite = 1;
+
+	labyrinthe->lesConnections[1].Gauche = 0; // Connexion avec 0
+	labyrinthe->lesConnections[1].Bas = 4;    // Connexion avec 4
+
+	labyrinthe->lesConnections[2].Bas = 5;
+
+	labyrinthe->lesConnections[3].Droite = 4;
+
+	labyrinthe->lesConnections[4].Haut = 1;   // Connexion avec 1
+	labyrinthe->lesConnections[4].Gauche = 3; // Connexion avec 3
+	labyrinthe->lesConnections[4].Bas = 7;    // Connexion avec 7
+
+	labyrinthe->lesConnections[5].Haut = 2;
+	labyrinthe->lesConnections[5].Bas = 8;
+
+	labyrinthe->lesConnections[6].Droite = 7;
+
+	labyrinthe->lesConnections[7].Haut = 4;   // Connexion avec 4
+	labyrinthe->lesConnections[7].Gauche = 6; // Connexion avec 6
+	labyrinthe->lesConnections[7].Droite = 8; // Connexion avec 8
+
+	labyrinthe->lesConnections[8].Gauche = 7;
+	labyrinthe->lesConnections[8].Haut = 5;
+
+	// Définir l'entrée et la sortie
+	labyrinthe->entree = 2;
+	labyrinthe->sortie = 8;
+
+	// Vérification des intersections et virages
+	if (ORD_estVirage(&labyrinthe->lesConnections[1], Est, labyrinthe, labyrinthe->largeur)) {
+		printf("Case 1 est un virage.\n");
+	}
+
+	if (ORD_estIntersection(&labyrinthe->lesConnections[4], labyrinthe)) {
+		printf("Case 4 est une intersection.\n");
+	}
+
+	if (ORD_estIntersection(&labyrinthe->lesConnections[7], labyrinthe)) {
+		printf("Case 7 est une intersection.\n");
+	}
+
+	if (ORD_estVirage(&labyrinthe->lesConnections[8], Nord, labyrinthe, labyrinthe->largeur)) {
+		printf("Case 8 est un virage.\n");
+	}
 
 	return labyrinthe;
 }
@@ -26,13 +68,15 @@ LAB_Labyrinthe* creerLabyrintheExemple() {
 void tester_ordresRobot() {
 	LAB_Labyrinthe* labyrinthe = creerLabyrintheExemple();
 
-	ORD_Position* chemin[4];
-	chemin[0] = &labyrinthe->lesConnections[1];  
-	chemin[1] = &labyrinthe->lesConnections[4];  
-	chemin[2] = &labyrinthe->lesConnections[5];  
-	chemin[3] = &labyrinthe->lesConnections[8];  
+	ORD_Position* chemin[5];
+	chemin[0] = &labyrinthe->lesConnections[2];  
+	chemin[1] = &labyrinthe->lesConnections[5];  
+	chemin[2] = &labyrinthe->lesConnections[8];  
+	chemin[3] = &labyrinthe->lesConnections[7];
+	chemin[4] = &labyrinthe->lesConnections[6];
 
-	unsigned int tailleChemin = 4;
+
+	unsigned int tailleChemin = 5;
 
 	ORD_Ordre* ordres = ORD_obtenirOrdres(chemin, tailleChemin, labyrinthe);
 
@@ -40,19 +84,20 @@ void tester_ordresRobot() {
 	for (unsigned int i = 0; ordres[i] != FIN; i++) {
 		switch (ordres[i]) {
 			case AV:
-				printf("AV (Avancer)\n");
+				printf("AV\n");
 				break;
 			case TG:
-				printf("TG (Tourner Gauche)\n");
+				printf("TG\n");
 				break;
 			case TD:
-				printf("TD (Tourner Droite)\n");
+				printf("TD\n");
 				break;
 			default:
 				break;
 		}
 	}
 	printf(".\n");
+
 	free(ordres);
 	free(labyrinthe->lesConnections);
 	free(labyrinthe);
