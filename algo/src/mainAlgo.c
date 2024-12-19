985d1922f9411d3f88c8c3a12ba3a720b880245f
nlevasseur <nathan.levasseur@inssa-rouen.fr>
2024-12-19 09:44:45 +0100

    modif mainAlgo

--- algo/src/mainAlgo.c
+++ algo/src/mainAlgo.c
@@ -12,37 +12,37 @@
 int main(int argc, char** argv){
     LAB_Labyrinthe labyrinthe;
     CASE_Case* chemin;
-    ORD_Ordre* listeDOrdre;
+    ORD_Ordre listeDOrdre[] = {AV,TG,AV,TD,AV,TG,AV,TD,AV,AV,TD,AV,TG,AV,FIN};
 
     uint i = 0;
     bool done = false;
 
     if(argc != 2){
-        printf("Le fichier non precise\n"); 
-        return EXIT_FAILURE;
+        //printf("Le fichier non precise\n"); 
+        //return EXIT_FAILURE;
         
     }
     char* path = argv[1];
     if(access(path, R_OK)!=0){
-        printf("Le fichier n'existe pas\n"); 
-        return EXIT_FAILURE; 
+        //printf("Le fichier n'existe pas\n"); 
+        //return EXIT_FAILURE; 
     }
     
-    labyrinthe = initialisationLabyrinthe(path);
+    //labyrinthe = initialisationLabyrinthe(path);
     //chemim = CHM_chemin(labyrinthe)
-    listeDOrdre = ORD_obtenirOrdres(&chemin, CHM_obtenirLongueur(chemin), LAB_obtenirLargeur(&labyrinthe));
+    //listeDOrdre[] = ;
     do{
-        switch(ORD_iemeOrdre(listeDOrdre, i)){
+        switch(listeDOrdre[i]){
             case AV :
-                printf("AV ");
+                printf("AV\n");
             case TD :
-                printf("TD ");
+                printf("TD\n");
                 break;
             case TG :
-                printf("TG ");
+                printf("TG\n");
                 break;
             case FIN :
-                printf(". ");
+                printf(".\n");
                 done = true;
                 break;
             default :
