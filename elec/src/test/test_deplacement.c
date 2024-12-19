c42d67fd24a1baa35d64985d0f8e68856226374c
tguenot <tom.guenot@insa-rouen.fr>
2024-12-19 08:21:52 +0100

    pull

--- elec/src/test/test_deplacement.c
+++ elec/src/test/test_deplacement.c
@@ -18,17 +18,8 @@ int main()
     printf("%d, %d\n", PWM_EN1, PWM_EN2);
     LCD_Init();
     MTR_initialisationMoteur(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
-
-    char texte1[17] = "Trouve";
-    char texte2[17]= "";
-    LCD_Write(texte2, texte2);
-    ROBOT_EtatDAvancement etat = Avancer;
-    ROBOT_avancer(&etat);
-    if (etat==Intersection)
-    {
-        LCD_Write(texte1, texte2);
-        printf("Trouvé !\n");
-        MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
-    }
+    MTR_avancer(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4, PWM_EN1, PWM_EN2);
+    delayMicroseconds(3000000);
+    MTR_arreter(MOTEUR_IN1, MOTEUR_IN2, MOTEUR_IN3, MOTEUR_IN4);
     return 0;
 }
